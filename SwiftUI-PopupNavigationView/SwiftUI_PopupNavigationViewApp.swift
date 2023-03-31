b20c838431c655c9cab413cf564c84aefb0c9a96
Ikmal Azman <opikmalazman@gmail.com>
2023-03-31 16:17:01 +0100

    Initial Commit

---
+++ SwiftUI-PopupNavigationView/SwiftUI_PopupNavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PopupNavigationViewApp.swift
+//  SwiftUI-PopupNavigationView
+//
+//  Created by Ikmal Azman on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PopupNavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
